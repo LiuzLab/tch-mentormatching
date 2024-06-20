6876ebd5d58913beffe804d86308a1e414bfd770
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2024-06-20 17:29:49 -0500

    linting

--- bin/build_index.py
+++ bin/build_index.py
@@ -11,15 +11,12 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 load_dotenv()
 PATH_TO_SUMMARY = "./simulated_data/mentor_student_cvs_with_summaries.csv"
 OPENAI_KEY = os.getenv("OPENAI_API_KEY")
-MODEL_NAME = "gpt-3.5-turbo-0125" # will change it :)
+MODEL_NAME = "gpt-3.5-turbo-0125"  # will change it :)
 
 
 def main():
     llm = ChatOpenAI(model=MODEL_NAME)
-    loader = CSVLoader(
-        file_path = PATH_TO_SUMMARY,
-        source_column="Mentor_Summary"
-    )
+    loader = CSVLoader(file_path=PATH_TO_SUMMARY, source_column="Mentor_Summary")
     docs = loader.load()
     vector_store = FAISS.from_documents(documents=docs, embedding=OpenAIEmbeddings())
     retriver = vector_store.as_retriever()
